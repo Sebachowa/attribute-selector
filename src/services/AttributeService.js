1a83bbaa049adc1cb57c8a301b2c7c8b52b3181b
AlvaroPareja <alvaro.pareja.arroyo@met.ey.com>
2021-10-22 11:38:14 +0200

    update in reunion2

--- src/services/AttributeService.js
+++ src/services/AttributeService.js
@@ -1,16 +1,34 @@
-/* eslint-disable class-methods-use-this */
 class AttributeService {
-  // constructor() {}
-
-  getInitialStats() {
+  static getInitialStats() {
     return {
-      tank: {
+      maxPoints: 30,
+      minPointsByStat: 1,
+      maxPointsByStat: 10,
+      characters: [{
         name: 'Tank',
         stats: [
           { name: 'STR', value: 3 },
-          { name: 'STR', value: 3 },
+          { name: 'DEX', value: 3 },
+          { name: 'CON', value: 3 },
+          { name: 'LUC', value: 3 },
         ],
-      },
+      }, {
+        name: 'Assassin',
+        stats: [
+          { name: 'STR', value: 3 },
+          { name: 'DEX', value: 3 },
+          { name: 'CON', value: 3 },
+          { name: 'LUC', value: 3 },
+        ],
+      }, {
+        name: 'Wizard',
+        stats: [
+          { name: 'STR', value: 3 },
+          { name: 'DEX', value: 3 },
+          { name: 'CON', value: 3 },
+          { name: 'LUC', value: 3 },
+        ],
+      }],
     };
   }
 }
