09adf094a0fd60cbe7659ce2ffe3b8fae1be63b8
Abraham <abraham.fernandez@eydevelopment.com>
2021-10-22 12:36:30 +0200

    change data initial

--- src/services/AttributeService.js
+++ src/services/AttributeService.js
@@ -1,34 +1,87 @@
 class AttributeService {
-  static getInitialStats() {
+  // eslint-disable-next-line class-methods-use-this
+  getInitialStats() {
     return {
       maxPoints: 30,
       minPointsByStat: 1,
       maxPointsByStat: 10,
-      characters: [{
-        name: 'Tank',
-        stats: [
-          { name: 'STR', value: 3 },
-          { name: 'DEX', value: 3 },
-          { name: 'CON', value: 3 },
-          { name: 'LUC', value: 3 },
-        ],
-      }, {
-        name: 'Assassin',
-        stats: [
-          { name: 'STR', value: 3 },
-          { name: 'DEX', value: 3 },
-          { name: 'CON', value: 3 },
-          { name: 'LUC', value: 3 },
-        ],
-      }, {
-        name: 'Wizard',
-        stats: [
-          { name: 'STR', value: 3 },
-          { name: 'DEX', value: 3 },
-          { name: 'CON', value: 3 },
-          { name: 'LUC', value: 3 },
-        ],
-      }],
+      characters: [
+        {
+          name: 'Assassin',
+          stats: [
+            {
+              name: 'STR',
+              value: 9,
+            },
+            {
+              name: 'VIT',
+              value: 2,
+            },
+            {
+              name: 'DEF',
+              value: 1,
+            },
+            {
+              name: 'DEX',
+              value: 9,
+            },
+            {
+              name: 'LUCK',
+              value: 9,
+            },
+          ],
+        },
+        {
+          name: 'Knight',
+          stats: [
+            {
+              name: 'STR',
+              value: 6,
+            },
+            {
+              name: 'VIT',
+              value: 6,
+            },
+            {
+              name: 'DEF',
+              value: 6,
+            },
+            {
+              name: 'DEX',
+              value: 6,
+            },
+            {
+              name: 'LUCK',
+              value: 6,
+            },
+          ],
+        },
+        {
+          name: 'Tank',
+          stats: [
+            {
+              name: 'STR',
+              value: 5,
+            },
+            {
+              name: 'VIT',
+              value: 10,
+            },
+            {
+              name: 'DEF',
+              value: 10,
+            },
+            {
+              name: 'DEX',
+              value: 2,
+            },
+            {
+              name: 'LUCK',
+              value: 3,
+            },
+          ],
+        },
+      ],
     };
   }
 }
