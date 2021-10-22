3494f38167bcb80d6a792972b0325c56c140c4fa
Abraham <abraham.fernandez@eydevelopment.com>
2021-10-22 12:38:14 +0200

    Basics Styles

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <BaseCard :data="tank"/>
+  <div id="app" class="container">
+    <BaseCard v-for="(fighter, index) in fighters" :key="index" :data="fighter"/>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
-  data: () => (
-    {
-      tank: {
-        name: 'Tank',
-        stats: [
-          { name: 'STR', value: 3 },
-          { name: 'STR', value: 3 },
-        ],
-      },
-    }
-  ),
   created() {
     this.$store.dispatch('getInitialStats');
   },
   components: {
     BaseCard,
+  },
+  computed: {
+    ...mapGetters(['fighters']),
   },
 };
 </script>
@@ -38,5 +30,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  grid-column-gap: 0px;
+  grid-row-gap: 32px;
+  @media only screen and (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 1fr;
+    grid-column-gap: 32px;
+    grid-row-gap: 0px;
+  }
 }
 </style>
